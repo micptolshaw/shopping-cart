7c9f057e756620db62c5e9f979f4614746ae3ce5
Michael Shaw <michael.shaw@plexure.com>
2021-12-06 21:10:39 +1300

    ADD: ShoppingCart Page

---
+++ src/ClientApp/src/app/shopping-cart/shopping-cart.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-cart',
+  templateUrl: './shopping-cart.component.html'
+})
+
+export class ShoppingCartComponent {
+}
